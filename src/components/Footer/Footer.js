7eb772852bba44d8bd3ffa9ce7050ba675a81f0f
“[newajsharif91]” <[email>
2022-10-27 21:43:21 +0600

    colour footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
           <a className="link link-hover">Marketing</a>
           <a className="link link-hover">Advertisement</a>
         </div>
-        <div>
+        <div className="text-white">
           <span className="footer-title">Company</span>
           <a className="link link-hover">About us</a>
           <a className="link link-hover">Contact</a>
